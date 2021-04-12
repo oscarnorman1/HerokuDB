9395beb20cd4c56e58c1327bf0d57a09dec84a3c
Oscar Norman <oscar.norman95@gmail.com>
2021-04-12 14:53:25 +0200

    project started

---
+++ src/main/java/com/testdb/herokudbtest/HerokuDbtestApplication.java
@@ -0,0 +1,13 @@
+package com.testdb.herokudbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuDbtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testdb/herokudbtest/HerokuDbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdb.herokudbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
